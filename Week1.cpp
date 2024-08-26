6620beb8c3a0960ca7c272bd9b1912cb534792ec
taeanasalinas <157439975+taeanasalinas@users.noreply.github.com>
2024-08-26 02:48:58 +0000

    Final Commit

--- Week1.cpp
+++ Week1.cpp
@@ -7,41 +7,75 @@ using namespace std;
 int main () {
 
     ifstream inFile ("student_data.txt");
-    int size = 53;
+    int size = 52;
+    string name[size];
     int age[size];
     float gpa[size];
-    string name[size];
     string line;
-    
+    double sum, avg;
+    bool firstNumber = true;
+    float max;
+    int oldest, youngest;
+    oldest = age[size];
+    youngest = age[size];
+     
 
-    if (inFile) {
-            cerr <<  "Error" << endl;
+    for (int i = 0; i < size; i++) {
+        getline(inFile, line);
+        inFile >> name[i] >> age[i] >> gpa[i];
 
-            
+        sum = sum + gpa[i];   
+        avg = sum / size;
+        }
+
+        cout << "Average GPA: " << avg << endl; 
+     inFile.close();   
+
+    for (int i = 0; i < size; i++) {
+        getline(inFile, line);
+        inFile >> name[i] >> age[i] >> gpa[i];
+    {
+        if (gpa[i] > max){
+            max = gpa[i];
+        }
     }
+        
+        }
+
+        cout << "Highest GPA: " << max << endl; 
+     inFile.close();  
+            
+    for (int i = 0; i < size; i++) {
+        getline(inFile, line);
+        inFile >> name[i] >> age[i];
+            if (age[i] > oldest)
+        {
+            oldest = age[i];
+        }
+        
+        cout << "Oldest: " << oldest << name[i] << endl;
+        
+        {break;}
+    }
+        
 
     for (int i = 0; i < size; i++) {
         
+        inFile >> name[i] >> age[i];
         getline(inFile, line);
-        
-        inFile >> age[i];
 
-        name[i] = line;
+        if (age[i] < youngest)
+        {
+            youngest = age[i];
+        }
 
-        getline(inFile, line);
+        cout << "Youngest: " << youngest << name[i] << endl;
+        {break;}
     }
-
-    inFile.close();
-
-    for (int i = 0; i < 52; i++)   {
-        
-       cout << name[i] << ": ";
-       cout << gpa[i] << endl;
-
+inFile.close();
     
-    }
-
-    return 0;
+       
+        return 0;
 }
 
 
